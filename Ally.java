92b7fdecbdae9783aa787656e722f4de77f16146
kuruneru <131349714+kuruneru@users.noreply.github.com>
2025-05-30 18:23:07 +0900

    Add files via upload

---
+++ Ally.java
@@ -0,0 +1,7 @@
+class Ally {
+
+    /* コンストラクタ(IDも設定) */
+    public Ally(String name, int hp, int atk) {
+        
+    }
+}
---
+++ Character.java
@@ -0,0 +1,26 @@
+class Character {
+    /* コンストラクタ */
+    public Character(String name, int hp, atk) {
+
+    }
+
+    /* atk分だけ減少させるダメージ処理 */
+    public void damege(int atk) {
+
+    }
+
+    /* 指定されたキャラへの攻撃 */
+    public attack(Character c) {
+
+    }
+
+    /* ステータスの表示 */
+    public void showStatus() {
+
+    }
+
+    /* 上昇した攻撃をもとに戻す */
+    public void resetStatus() {
+
+    }
+}
